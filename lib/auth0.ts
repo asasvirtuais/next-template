4552796357fae59355492aed5bef57a54fdd0be1
Icaro Asas Virtuais <asasvirtuais.dev@gmail.com>
2026-02-25 15:54:49 -0300

    missed this

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,34 +1,20 @@
-import { create, list } from '@/app/interface'
 import { Auth0Client } from '@auth0/nextjs-auth0/server'
 import { redirect } from 'next/navigation'
 
 declare module '@auth0/nextjs-auth0/server' {
-  interface User {
-    id: string
-  }
+    interface User {
+        id: string
+    }
 }
 
 export const auth0 = new Auth0Client({
     async beforeSessionSaved(session) {
         const user = session.user
 
-        if (! user)
-            throw new Error('No user in session')
-        let appUser
-        const [found] = await list({query: { sub: user.sub }})
-
-        if (found)
-            appUser = found
-        else
-            appUser = await create({data: { name: user.name as string, sub: user.sub }})
-
-        if (!appUser)
-            throw new Error('Failed to create profile')
-
         return {
             ...session,
             user: {
-                id: appUser.id,
+                id: user.sub,
                 ...user,
             }
         }
@@ -37,7 +23,7 @@ export const auth0 = new Auth0Client({
 
 export async function authenticate() {
     const session = await auth0.getSession()
-    if (! session?.user)
+    if (!session?.user)
         return redirect('/auth/login')
     return session.user
 }
